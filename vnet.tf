9f03d79882c99c6b40b4c36b66d6b9943e6d1574
Robert Scully <robert.p.scully@gmail.com>
2021-06-29 15:40:34 +0000

    Adding required variable to vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network-acmerobb" {
   source  = "iacdev.wpp.cloud/WPP-Lab/network-acmerobb/azurerm"
   version = "3.0.1"
-  # insert required variables here
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
